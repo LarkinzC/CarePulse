5ed389900553d724b346388996cce99877aee8d3
Larkin Campbell <Larkincampbell24@gmail.com>
2024-09-29 20:10:24 -0700

    defining custom field in Custom Form Fields

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -7,6 +7,16 @@ import { Button } from "@/components/ui/button"
 import {Form} from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import CustomFormField from "../CustomFormField"
+
+export enum FormFieldType {
+  INPUT = 'input',
+  TEXTAREA = 'textarea',
+  PHONE_INPUT = 'phoneInput',
+  CHECKBOX = 'checkbox',
+  DATE_PICKER = 'datePicker',
+  SELECT = 'select',
+  SKELETON = 'skeleton',
+}
  
 const formSchema = z.object({
   username: z.string().min(2, {
@@ -34,7 +44,15 @@ const PatientForm =() => {
         <h1 className="header">Hi there 👋</h1>
         <p className="text-dark-700">Schedule your first appointment</p>
       </section>
-      <CustomFormField control={form.control}/>
+      <CustomFormField 
+      fieldType={FormFieldType.INPUT}
+      control={form.control}
+      name='name'
+      label='Full name'
+      placeholder= 'John Doe'
+      iconSrc='/assets/icons/user.svg'
+      iconAlt='user'
+      />
       <Button type="submit">Submit</Button>
     </form>
   </Form>
