89451ad8f88e13896b5a36fc62d60c05a7603105
Larkin Campbell <Larkincampbell24@gmail.com>
2024-10-06 17:36:13 -0700

    input case solved

--- components/CustomFormField.tsx
+++ components/CustomFormField.tsx
@@ -11,6 +11,7 @@ import {
   import { Input } from "@/components/ui/input"
   import { Control } from 'react-hook-form'
   import { FormFieldType } from "./forms/PatientForm"
+  import Image from "next/image"
 
 
   interface CustomProps {
@@ -30,8 +31,30 @@ import {
   }
 
 const RenderField = ({field, props }: { field: any; props: CustomProps}) => {
-    switch (props.fieldType) {
+    const { fieldType,iconSrc,iconAlt, placeholder, } = props
+
+    switch (fieldType) {
         case FormFieldType.INPUT:
+            return (
+                <div className="flex rounded-md border border-dark-500 bg-dark-400">
+                    {iconSrc && (
+                        <Image 
+                            src={iconSrc}
+                            height={24}
+                            width={24}
+                            alt={iconAlt || 'icon'}
+                            className="ml-2"
+                        />
+                    )}
+                    <FormControl>
+                        <Input 
+                            placeholder={placeholder}
+                            {...field}
+                            className="shad-input border-0"
+                        />
+                    </FormControl>
+                </div>
+            )
 
         break
 
