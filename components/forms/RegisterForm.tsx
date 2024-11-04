81f32394454bec94328eed235518568a67fd9000
Larkin Campbell <Larkincampbell24@gmail.com>
2024-11-04 15:04:23 -0800

    all personal information fields

--- components/forms/RegisterForm.tsx
+++ components/forms/RegisterForm.tsx
@@ -123,11 +123,7 @@ const RegisterForm = ({user}: { user: User}) => {
       )}
       />
       </div>
-      {/* <section className="mb-12 space-y-6">
-        <div className="mb-9 space-y-1">
-            <h2 className="sub-header">Medical information</h2>
-        </div>
-      </section> */}
+      
       <div className="flex flex-col gap-6 xl:flex-row">
       <CustomFormField 
       fieldType={FormFieldType.INPUT}
@@ -147,8 +143,26 @@ const RegisterForm = ({user}: { user: User}) => {
 
       </div>
       <div className="flex flex-col gap-6 xl:flex-row">
-          
+      <CustomFormField 
+      fieldType={FormFieldType.INPUT}
+      control={form.control}
+      name='emergencyContactName'
+      label='Emergency contact name'
+      placeholder= "Guardian's name"
+      />
+      <CustomFormField 
+      fieldType={FormFieldType.PHONE_INPUT}
+      control={form.control}
+      name='emergencyContactNumber'
+      label='Emergency contact number'
+      placeholder= '(999) 123-4567'
+      />
       </div>
+      <section className="mb-12 space-y-6">
+        <div className="mb-9 space-y-1">
+            <h2 className="sub-header">Medical information</h2>
+        </div>
+      </section>
       <div className="flex flex-col gap-6 xl:flex-row">
           
       </div>
