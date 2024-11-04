666e36018934da3571bd56fe426e2bcc3398feda
Larkin Campbell <Larkincampbell24@gmail.com>
2024-11-04 15:01:54 -0800

    occupation and address field added

--- components/forms/RegisterForm.tsx
+++ components/forms/RegisterForm.tsx
@@ -116,14 +116,35 @@ const RegisterForm = ({user}: { user: User}) => {
                   {option}
                 </Label>
               </div>
+              
             ))}
           </RadioGroup>
         </FormControl>
       )}
       />
       </div>
+      {/* <section className="mb-12 space-y-6">
+        <div className="mb-9 space-y-1">
+            <h2 className="sub-header">Medical information</h2>
+        </div>
+      </section> */}
       <div className="flex flex-col gap-6 xl:flex-row">
-          
+      <CustomFormField 
+      fieldType={FormFieldType.INPUT}
+      control={form.control}
+      name='address'
+      label='Address'
+      placeholder= '14th St., New York'
+      />
+      <CustomFormField 
+      fieldType={FormFieldType.INPUT}
+      control={form.control}
+      name='occupation'
+      label='Occupation'
+      placeholder= 'Software Engineer'
+      />
+
+
       </div>
       <div className="flex flex-col gap-6 xl:flex-row">
           
