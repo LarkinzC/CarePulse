df7538e60b53fdea06fbbc00f2b9bb1932b88945
Larkin Campbell <Larkincampbell24@gmail.com>
2024-12-11 17:45:37 -0800

    adjusting structure and styles of new page

--- app/patients/[userId]/new-appointment/page.tsx
+++ app/patients/[userId]/new-appointment/page.tsx
@@ -1,9 +1,35 @@
-import React from 'react'
 
-const NewAppointment = () => {
+import Image from 'next/image'
+import PatientForm from "@/components/forms/PatientForm";
+import Link from 'next/link'
+
+export default function NewAppointment() {
   return (
-    <div>NewAppointment</div>
-  )
+    <div className="flex h-screen max-h-screen">
+      {/* TODO: OTP verification */}
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[860px] flex-1 justify-between">
+          <Image 
+          src='/assets/icons/logo-full.svg'
+          height={1000}
+          width={1000}
+          alt="patient"
+          className="mb-12 h-10 w-fit"
+          />
+
+          {/* <PatientForm /> */}
+
+          
+          <p className="justify-items-end text-dark-600 xl:text-left">© 2024 CarePulse</p>
+        </div>
+      </section>
+      <Image src='/assets/images/appointment-img.png'
+        height={1000}
+        width={1000}
+        alt="appointment"
+        className="side-img max-w-[390px] bg-bottom"/>
+    </div>
+  );
 }
 
-export default NewAppointment
+
