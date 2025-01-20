376667e36fe17be902b819f57112f31b86049ac6
Larkin Campbell <Larkincampbell24@gmail.com>
2025-01-19 16:18:00 -0800

    fetch appointment function

--- app/patients/[userId]/new-appointment/success/page.tsx
+++ app/patients/[userId]/new-appointment/success/page.tsx
@@ -2,7 +2,11 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
-const Success = () => {
+const Success = ({}) => {
+
+
+
+
   return (
     <div className='flex h-screen max-h-screen px[5%]'>
         <div className='success-img'>
@@ -24,6 +28,15 @@ const Success = () => {
             <h2 className='header mb-6 max-w-[600px] text-center'>
               Your <span className='text-green-500'>appointment request</span> has been succesfully submitted!
             </h2>
+            <p>We will be in touch shortly to confirm your appointment.</p>
+            </section>
+            <section className='request-details'>
+              <p>Requested appointment details:</p>
+              <div className='flex items-center gap-3'>
+                <Image 
+                
+                />
+              </div>
             </section>
         </div>
     </div>
