233e1ecab28fbff0762456a2053ca7ada648593b
Larkin Campbell <Larkincampbell24@gmail.com>
2025-01-27 20:00:14 -0800

    implementing useRouter

--- components/PasskeyModal.tsx
+++ components/PasskeyModal.tsx
@@ -11,10 +11,18 @@ import {
     AlertDialogTitle,
     AlertDialogTrigger,
   } from "@/components/ui/alert-dialog"
+import Image from "next/image"
+import { useRouter } from "next/navigation"
   import { useState } from "react"
 
 const PasskeyModal = () => {
+    const router = useRouter()
     const [open, setOpen] = useState(true)
+    
+    const closeModal = () => {
+        setOpen(false)
+        router.push('/')
+    }
 
   return (
     <AlertDialog open={open} onOpenChange={setOpen}>
@@ -22,6 +30,13 @@ const PasskeyModal = () => {
     <AlertDialogHeader>
       <AlertDialogTitle className="flex items-start justify-between">
         Admin Access Verification
+        <Image 
+            src='/assets/icons/close.svg'
+            alt="close"
+            width={20}
+            height={20}
+            onClick={() => closeModal()}
+        />
       </AlertDialogTitle>
       <AlertDialogDescription>
         This action cannot be undone. This will permanently delete your account
