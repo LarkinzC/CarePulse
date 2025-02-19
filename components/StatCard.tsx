ac24a46818e22262b9bb735b476f89ac14afc2d7
Larkin Campbell <Larkincampbell24@gmail.com>
2025-02-18 18:15:09 -0800

    stat cards nearly complete

--- components/StatCard.tsx
+++ components/StatCard.tsx
@@ -1,8 +1,31 @@
+import clsx from 'clsx'
+import Image from 'next/image'
 import React from 'react'
 
-const StatCard = () => {
+interface StatCardProps {
+    type: 'appointments' | 'pending' | 'cancelled'
+    count: number
+    label: string
+    icon: string
+}
+
+const StatCard = ({ count = 0, label, icon, type}: StatCardProps) => {
   return (
-    <div>StatCard</div>
+    <div className={clsx('stat-card', {
+        'bg-appointments': type === 'appointments',
+        'bg-pending': type === 'pending',
+        'bg-cancelled': type === 'cancelled'
+    })}>
+        <div className='flex items-center gap-4'>
+            <Image 
+            src={icon}
+            height={32}
+            width={32}
+            alt='label'
+            className='size-8 w-fit'
+            />
+        </div>
+    </div>
   )
 }
 
