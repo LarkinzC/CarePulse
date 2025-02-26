5e5312f983d98c5bf8c388cc0b898c0a6f173b24
Larkin Campbell <Larkincampbell24@gmail.com>
2025-02-25 20:59:05 -0800

    initial frame of query counter laid out

--- lib/actions/appointment.actions.ts
+++ lib/actions/appointment.actions.ts
@@ -3,6 +3,7 @@
 import { APPOINTMENT_COLLECTION_ID, DATABASE_ID, databases } from "../appwrite.config"
 import { ID } from "node-appwrite"
 import { parseStringify } from "../utils"
+import { Appointment } from "@/types/appwrite.types"
 
 export const createAppointment = async (appointment: CreateAppointmentParams) => {
     try {
@@ -27,6 +28,39 @@ export const getAppointment = async (appointmentId: string) => {
         )
 
         return parseStringify(appointment)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export const getRecentAppointmentsList = async () => {
+    try {
+        const appointments = await databases.listDocuments(
+            DATABASE_ID!,
+            APPOINTMENT_COLLECTION_ID!,
+            [Query.orderDesc('$createdAt')]
+        ) 
+
+        const initialCounts = {
+            scheduledCount: 0,
+            pendingCount: 0,
+            cancelledCount: 0,
+        }
+
+        const counts = (appointments.documents as Appointment[]).reduce((acc, appointment) => {
+
+            if (appointment.status === 'scheduled') {
+                acc.scheduledCount += 1
+            } else if (appointment.status === 'cancelled') {
+                acc.cancelledCount += 1
+            } else if (appointment.status === 'pending') {
+                acc.pendingCount += 1
+            }
+
+            return acc
+            
+
+        }, initialCounts)
     } catch (error) {
         console.log(error)
     }
