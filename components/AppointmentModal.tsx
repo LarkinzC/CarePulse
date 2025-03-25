94374ddd3ff7e101e523d69feb8e2502a0607398
Larkin Campbell <Larkincampbell24@gmail.com>
2025-03-25 15:26:28 -0700

    building dialogue buttons

--- components/AppointmentModal.tsx
+++ components/AppointmentModal.tsx
@@ -1,8 +1,38 @@
-import React from 'react'
+"use client"
 
-const AppointmentModal = () => {
+import { useState } from 'react'
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+  } from "@/components/ui/dialog"
+import { Button } from './ui/button'
+
+const AppointmentModal = ({ type }: {
+    type: 'schedule' | 'cancel'
+}) => {
+
+    const [open, setOpen] = useState(false)
   return (
-    <div>AppointmentModal</div>
+    <Dialog open={open} onOpenChange={setOpen}>
+  <DialogTrigger>
+    <Button variant='ghost' className={`capitalize ${type === 'schedule' && 'text-green-500'}`}>
+        {type}
+    </Button>
+  </DialogTrigger>
+  <DialogContent className='shad-dialogue sm:max-w-md'>
+    <DialogHeader className='mb-4 space-y-3'>
+      <DialogTitle className='capitalize'>{type} Appointment</DialogTitle>
+      <DialogDescription>
+        Please fill in the following details to {type} an appointment
+      </DialogDescription>
+    </DialogHeader>
+    
+  </DialogContent>
+</Dialog>
   )
 }
 
