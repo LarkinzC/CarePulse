c823d27ca93691897661dc2c913939b939fa7c18
Larkin Campbell <Larkincampbell24@gmail.com>
2025-04-16 03:15:09 -0700

    removed payment typing from appointment

--- components/table/columns.tsx
+++ components/table/columns.tsx
@@ -30,14 +30,8 @@ import AppointmentModal from "../AppointmentModal"
 
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
-export type Payment = {
-  id: string
-  amount: number
-  status: "pending" | "processing" | "success" | "failed"
-  email: string
-}
 
-export const columns: ColumnDef<Payment>[] = [
+export const columns: ColumnDef<Appointment>[] = [
   {
     header: 'ID',
     cell:({ row }) => <p className="text-14-medium">{row.index + 1}</p>
